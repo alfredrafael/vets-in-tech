b71cf38c28c872b89cb97e683e1980e1d3b0aefc
alfredrafael <alfredrafael@gmail.com>
2019-05-19 04:55:48 -0400

    Adds personal logo to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,23 +19,23 @@ class Footer extends React.Component {
             <Row>
                 <Col>
                     <ul className="footer-nav">
-                        <li><a href="http://about.draftkings.com/tech-for-heroes/">Draft Kings |</a></li>
-                        <li><a href="#">Vets In Teach |</a></li>
-                        <li><a href="#">General Assembly</a></li>
+                        <li><a href="http://about.draftkings.com/tech-for-heroes/" target="_blank">Draft Kings |</a></li>
+                        <li><a href="https://vetsintech.co/" target="_blank">Vets In Teach |</a></li>
+                        <li><a href="https://generalassemb.ly/locations/boston" target="_blank">General Assembly</a></li>
                     </ul>
                 </Col>
                 <Col>
                     <ul className="social-links">
-                        <li><a href="https://www.linkedin.com/company/vetsintech/"><i className="fab fa-linkedin"></i></a></li>
-                        <li><a href="https://www.facebook.com/veteransintech/"><i className="fab fa-facebook-square"></i></a></li>
-                        <li><a href="https://twitter.com/VetsInTech"><i className="fab fa-twitter-square"></i></a></li>
+                        <li><a href="https://www.linkedin.com/company/vetsintech/" target="_blank"><i className="fab fa-linkedin"></i></a></li>
+                        <li><a href="https://www.facebook.com/veteransintech/" target="_blank"><i className="fab fa-facebook-square"></i></a></li>
+                        <li><a href="https://twitter.com/VetsInTech" target="_blank"><i className="fab fa-twitter-square"></i></a></li>
 
                     </ul>
                 </Col>
             </Row>
             <Row>
-                <p>
-                    Copyright &copy; {currentYear} by Alfredo R. Pabon. All rights reserved.
+                <p className='signature'>
+                    Copyright &copy; {currentYear} by &nbsp; <a href="https://alfredorafael.com/"><img src="https://i.imgur.com/6BvcC6F.png" style={{maxWidth: '12%'}}/></a> &nbsp; .All rights reserved.
                 </p>
             </Row>
         </Container>
